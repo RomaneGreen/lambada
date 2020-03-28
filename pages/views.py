bbe2db37257a066016ea1c548454f41bd6587ccc
Romane G <romaneg@Romanes-MacBook-Pro.local>
2020-03-28 16:35:22 -0400

    add and run static files via collectstatic

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request,'pages/index.html')
+
+
+def about(request):
+    return render(request,'pages/about.html')
 
 
