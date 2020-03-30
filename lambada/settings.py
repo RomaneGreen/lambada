063d960f9acf73e0c02472fc2ddb4b6d2d715f51
Romane G <romaneg@Romanes-MacBook-Pro.local>
2020-03-29 20:37:36 -0400

    linked database

--- lambada/settings.py
+++ lambada/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'lambada.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lambadadb',
+        'USER': 'romaneg',
+        'PASSWORD': 'rkgrkgrkg',
+        'HOST': 'localhost'
     }
 }
 
