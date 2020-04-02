903ddf97e84f6fd1a8e8892ee5c548dd4978b809
Romane G <romaneg@Romanes-MacBook-Pro.local>
2020-04-02 18:38:26 -0400

    customize admin

--- lenders/admin.py
+++ lenders/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Lender
-admin.site.register(Lender)
+
+
+class LenderAdmin(admin.ModelAdmin):
+    list_display = ('id','name','location','phone')
+    list_display_links = ('id','name')
+    list_filter = ('name',)
+    search_fields = ('id','name','location','phone')
+    list_per_page = 25
+
+
+admin.site.register(Lender,LenderAdmin)
--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,13 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Listing
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id','title','program','address','amount','is_published')
+    list_display_links = ('id','title')
+    list_filter = ('state',)
+    list_editable = ('is_published',)
+    search_fields = ('title','address','program','city')
+    list_per_page = 25
+
+admin.site.register(Listing,ListingAdmin)
