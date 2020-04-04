3500bb28a13914f3fe720c025485c209034e595b
Romane G <romaneg@Romanes-MacBook-Pro.local>
2020-04-04 16:21:59 -0400

    single listings page

--- listings/views.py
+++ listings/views.py
@@ -1,21 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Listing
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 
 
 def index(request):
-   listings = Listing.objects.all()
-   
+   listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+   paginator = Paginator(listings, 1)
+   page = request.GET.get('page')
+   paged_listings = paginator.get_page(page)
+
    context = {
-       'listings': listings
+       'listings': paged_listings
    }
+
+ 
 
    return render(request,'listings/listings.html',context)
 
 
 
 def listing(request, listing_id):
-   return render(request,'listings/listing.html')
+  listing = get_object_or_404(Listing, pk=listing_id)
+
+  context = {
+      'listing': listing
+  }
+  return render(request,'listings/listing.html',context)
 
 
 
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from listings.models import Listing
+from lenders.models import Lender
 
 def index(request):
-    return render(request,'pages/index.html')
+
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    context = {
+        'listings': listings
+    }       
+    return render(request,'pages/index.html',context)
 
 
 def about(request):
-    return render(request,'pages/about.html')
+    lenders = Lender.objects.order_by('-programdate')
+
+    #get lender 
+    mvp_lenders = Lender.objects.all().filter(is_mvp=True)
+
+    context = {
+        'lenders': lenders,
+        'mvp_lenders': mvp_lenders
+    } 
+    return render(request,'pages/about.html',context)
 
 
