33fab503ac4f65dd7a5923e30d19643b0d259b87
Romane G <romaneg@Romanes-MacBook-Pro.local>
2020-04-04 23:33:13 -0400

    search filters

--- listings/views.py
+++ listings/views.py
@@ -34,11 +34,41 @@ def listing(request, listing_id):
 
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    if 'keywords' in request.GET:
+      keywords = request.GET['keywords']
+      if keywords:
+        queryset_list = queryset_list.filter(description__icontains=keywords)
+      
+    if 'city' in request.GET:
+        keywords = request.GET['city']
+        if keywords:
+            queryset_list = queryset_list.filter(city__iexact=keywords) 
+    
+    if 'state' in request.GET:
+        keywords = request.GET['state']
+        if keywords:
+            queryset_list = queryset_list.filter(state__iexact=keywords)  
+    
+    if 'bedrooms' in request.GET:
+        keywords = request.GET['bedrooms']
+        if keywords:
+            queryset_list = queryset_list.filter(id__lte=keywords)   
+
+    if 'price' in request.GET:
+        keywords = request.GET['price']
+        if keywords:
+            queryset_list = queryset_list.filter(minimumcontribution__lte=keywords)  
+
+
+
     context = {
       
         'state_choices': state_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
+        'listings': queryset_list,
         'values': request.GET
         # 'listings': queryset_listing
     }  
