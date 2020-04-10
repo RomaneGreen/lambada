19a66e667c02611001e203272c04de76d4a67695
Romane G <romaneg@Romanes-MacBook-Pro.local>
2020-04-10 12:17:27 -0400

    maps on listing page

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,30 @@
-const date = new Date();
-document.querySelector('.year').innerHTML = date.getFullYear();
 
-
-
-setTimeout(() => {
-    $('message').fadeOut('slow')
+setTimeout(function() {
+  $('#message').fadeOut('slow');
 }, 3000);
+
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+   console.log("NOT SUPPORTED")
+  }
+}
+
+function showPosition(position) {
+
+  let lat = position.coords.latitude 
+  let lon = position.coords.longitude
+  console.log("COORDINATES",lat,lon);
+
+ 
+}
+
+function hey() {
+
+alert("hey")
+
+
+}
