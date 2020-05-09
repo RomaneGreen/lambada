332a6bc66a19751eca54f499b025fe5091ef9593
Romane Green <romane71193@gmail.com>
2020-05-08 23:58:17 -0400

    tidy css

--- accounts/models.py
+++ accounts/models.py
@@ -7,12 +7,12 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    occupation = models.TextField(max_length=200, blank=True)
-    employer = models.CharField(max_length=200, blank=True)
-    householdincome = models.CharField(max_length=200, blank=True)
-    familysize = models.CharField(max_length=200, blank=True)
-    veteran = models.CharField(max_length=100, blank=True)
-    firsttimebuyer = models.CharField(max_length=100, blank=True)
+    occupation = models.TextField(max_length=200, blank=True,verbose_name="Occupation")
+    employer = models.CharField(max_length=200, blank=True,verbose_name="Employer")
+    householdincome = models.CharField(max_length=200, blank=True,verbose_name="Annual Income")
+    familysize = models.CharField(max_length=200, blank=True,verbose_name="Family Size")
+    veteran = models.CharField(max_length=100, blank=True,verbose_name="Veteran?")
+    firsttimebuyer = models.CharField(max_length=100, blank=True,verbose_name="First Time Home Buyer?")
     def __str__(self):
         return self.user.username
 
